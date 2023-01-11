fd3d0cc447b166b0f40c26999dd2ee1f7b56ff63
Grzegorz Gardecki <gardecki.grzegorz@gmail.com>
2023-01-11 10:48:07 +0100

    API support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PlanetComponent } from './planet/planet.component';
+import { PlanetService } from './planet/planet.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
+
+// let id = number;
 export class AppComponent implements OnInit {
+  constructor(private planetService: PlanetService) {}
+
   ngOnInit() {
-    console.log('POJAWIŁEM SIE');
+    this.planetService.getPlanet(1).subscribe((planet) => {
+      console.warn(planet);
+    });
+  }
+  getPlanet() {
+    // implementation here
   }
 }
