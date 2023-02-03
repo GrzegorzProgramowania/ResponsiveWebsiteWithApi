6528e69404db2c2187fbc83b7fe803922e03285a
Grzegorz Gardecki <gardecki.grzegorz@gmail.com>
2023-02-03 16:08:26 +0100

    completion of data from film interface and filmPageLook component

--- src/app/pages/film-pages/film-page-look/film-page-look.component.ts
+++ src/app/pages/film-pages/film-page-look/film-page-look.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Film } from 'src/app/interfaces/film';
+import { FilmService } from 'src/app/services/film.service';
 
 @Component({
   selector: 'app-film-page-look',
   templateUrl: './film-page-look.component.html',
-  styleUrls: ['./film-page-look.component.scss']
+  styleUrls: ['./film-page-look.component.scss'],
 })
 export class FilmPageLookComponent {
+  id: number = null;
+  film: Film;
 
+  constructor(private route: ActivatedRoute, private filmService: FilmService) {
+    console.log(this.route.snapshot.params['id']);
+    this.id = this.route.snapshot.params['id'];
+    this.filmService.getFilm(this.id).subscribe((film) => {
+      this.film = film;
+      console.log('person', film);
+    });
+  }
 }
