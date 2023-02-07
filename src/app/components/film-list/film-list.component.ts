f4d5a625ba288f3b585b6debf179c6264384cd6b
Grzegorz Gardecki <gardecki.grzegorz@gmail.com>
2023-02-07 13:39:33 +0100

    add regex to filmListComp with bug

--- src/app/components/film-list/film-list.component.ts
+++ src/app/components/film-list/film-list.component.ts
@@ -1,11 +1,19 @@
-import { Component, Input } from '@angular/core';
-import { Film } from '../../interfaces/film';
+import { Component, Input } from "@angular/core";
+import { Film } from "../../interfaces/film";
 
 @Component({
-  selector: 'app-film-list',
-  templateUrl: 'film-list.component.html',
-  styleUrls: ['./film-list.component.scss'],
+  selector: "app-film-list",
+  templateUrl: "film-list.component.html",
+  styleUrls: ["./film-list.component.scss"],
 })
 export class FilmListComponent {
   @Input() films: Film[] = [];
+
+  getId(url: string): string {
+    console.log(url);
+    const regex = /^https:\/\/swapi\.dev\/api\/film\/(.+)\/$/;
+    const match = url.match(regex);
+    console.log("match", match);
+    return match[1];
+  }
 }
