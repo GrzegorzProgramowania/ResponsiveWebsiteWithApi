c89f07b37cdeee802941188bdfd22294338f37e5
Grzegorz Gardecki <gardecki.grzegorz@gmail.com>
2023-02-28 15:59:45 +0100

    removed the redundancies

--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -15,19 +15,3 @@ export class SettingsService {
     this.colorChanged.emit(this.colorWhite);
   }
 }
-
-// import { Injectable } from '@angular/core';
-
-// @Injectable({
-//   providedIn: 'root',
-// })
-// export class SettingsService {
-//   soundOn = false;
-//   colorWhite = false;
-
-//   constructor() {}
-
-//   toggleColor() {
-//     this.colorWhite = !this.colorWhite;
-//   }
-// }
