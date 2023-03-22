0b2dd6268a35ea07ec4cf8c03fe0ee0211ec4e74
Grzegorz Gardecki <gardecki.grzegorz@gmail.com>
2023-03-22 15:16:25 +0100

    added HostListener to app.comp to display the mp3 when clicking on the page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { SettingsService } from 'src/app/services/settings.service';
 
 @Component({
@@ -7,5 +7,13 @@ import { SettingsService } from 'src/app/services/settings.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  audio = new Audio('/assets/sounds/StarWars.mp3');
   constructor(public settingsService: SettingsService) {}
+
+  @HostListener('window:click', ['$event'])
+  onPageLoad() {
+    this.audio?.play().then(() => {
+      this.audio = null;
+    });
+  }
 }
